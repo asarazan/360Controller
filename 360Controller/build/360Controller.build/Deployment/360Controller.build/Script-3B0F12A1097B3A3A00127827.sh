c9b16a56fb66d5772e7a17ddc8810d526b9eea3b
Aaron Sarazan <aaron@sarazan.net>
2011-12-19 21:09:44 -0800

    remove build files

--- 360Controller/build/360Controller.build/Deployment/360Controller.build/Script-3B0F12A1097B3A3A00127827.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd "$TARGET_BUILD_DIR/$TARGET_NAME.kext/Contents"
-mkdir PlugIns > /dev/null 2>&1
-rm -R PlugIns/Feedback360.plugin > /dev/null 2>&1
-echo "$SRCROOT/../Feedback360/build/Deployment/Feedback360.bundle"
-cp -R "$SRCROOT/../Feedback360/build/Deployment/Feedback360.bundle" PlugIns/Feedback360.plugin
-# Renaming extension from bundle to a plugin? Could be naughty.
